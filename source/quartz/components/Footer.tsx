70096c2f6e3be0fe8099e07b79d37a50d69eeb41
Showyeab Ahmed <msashoyeb@gmail.com>
2024-10-20 00:04:50 +0600

    Footer removed

--- source/quartz/components/Footer.tsx
+++ source/quartz/components/Footer.tsx
@@ -7,25 +7,30 @@ interface Options {
   links: Record<string, string>
 }
 
+// export default ((opts?: Options) => {
+//   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
+//     const year = new Date().getFullYear()
+//     const links = opts?.links ?? []
+//     return (
+//       <footer class={`${displayClass ?? ""}`}>
+//         <p>
+//           {i18n(cfg.locale).components.footer.createdWith}{" "}
+//           <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
+//         </p>
+//         <ul>
+//           {Object.entries(links).map(([text, link]) => (
+//             <li>
+//               <a href={link}>{text}</a>
+//             </li>
+//           ))}
+//         </ul>
+//       </footer>
+//     )
+//   }
+
 export default ((opts?: Options) => {
   const Footer: QuartzComponent = ({ displayClass, cfg }: QuartzComponentProps) => {
-    const year = new Date().getFullYear()
-    const links = opts?.links ?? []
-    return (
-      <footer class={`${displayClass ?? ""}`}>
-        <p>
-          {i18n(cfg.locale).components.footer.createdWith}{" "}
-          <a href="https://quartz.jzhao.xyz/">Quartz v{version}</a> © {year}
-        </p>
-        <ul>
-          {Object.entries(links).map(([text, link]) => (
-            <li>
-              <a href={link}>{text}</a>
-            </li>
-          ))}
-        </ul>
-      </footer>
-    )
+    return null;
   }
 
   Footer.css = style
